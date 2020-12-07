d388d155374977a8b173737a08c577e4f99b5f32
Qetti <qetti>
2020-12-07 21:53:05 +0400

    feat: add homework4

---
+++ Homework4/homeworkN4/app/src/main/java/ge/msda/myapplication/fragments/SettingsFragment.kt
@@ -0,0 +1,8 @@
+package ge.msda.myapplication.fragments
+
+
+import androidx.fragment.app.Fragment
+import ge.msda.myapplication.R
+
+class SettingsFragment : Fragment(R.layout.fragment_settings) {
+}
